f34c9c8640011d27d62077a3709c935277069698
nandananair222 <nandananair222@gmail.com>
2025-11-06 10:16:37 +0530

    api.js

---
+++ api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api' });
+export function setAuthToken(token) {
+  if(token) api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  else delete api.defaults.headers.common['Authorization'];
+}
+export default api;
